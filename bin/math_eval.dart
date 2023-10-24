e32b08b565c4e711fe4c1c09ac52304f36a2fddd
Prog <progrodv@gmail.com>
2023-10-24 19:11:05 +0300

    Init project.

---
+++ bin/math_eval.dart
@@ -0,0 +1,5 @@
+import 'package:math_eval/math_eval.dart' as math_eval;
+
+void main(List<String> arguments) {
+  print('Hello world: ${math_eval.calculate()}!');
+}
---
+++ test/math_eval_test.dart
@@ -0,0 +1,8 @@
+import 'package:math_eval/math_eval.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
